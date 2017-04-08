68952b438da0d168b1ee0cf4b1a53ca452f1cdb9
Glyna <sampanggj@gmail.com>
2017-04-08 21:06:12 +0800

    project euler prob 1 solved

---
+++ src/gjexer/pje1.java
@@ -0,0 +1,18 @@
+package gjexer;
+
+public class pje1 {
+	public static void main (String args []){
+		int count = 1;
+		int total = 0;
+		while (count < 1000){
+				if ((count %3 )==0){
+					total = total + count;
+				} else if ((count %5)==0){
+					total = total + count;
+				}
+			count++;
+		}
+		System.out.print(total);
+	}
+	
+}
