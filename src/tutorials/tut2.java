559ac8013f18045a7630ed9af65f4471699881a1
Glyna <sampanggj@gmail.com>
2017-04-09 12:59:07 +0800

    added tut2 variables

---
+++ src/tutorials/tut2.java
@@ -0,0 +1,10 @@
+package tutorials;
+
+public class tut2 {
+	public static void main (String args []){
+		double tuna;
+		tuna = 5.28;
+		
+		System.out.println("This is a value of tuna: " + tuna);
+	}
+}
