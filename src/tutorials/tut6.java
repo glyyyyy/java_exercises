d5834b692b81a953f17df75fed96e1f425b081af
Glyna <sampanggj@gmail.com>
2017-04-09 23:32:04 +0800

    added tut6

---
+++ src/tutorials/tut6.java
@@ -0,0 +1,14 @@
+package tutorials;
+import java.util.Scanner;
+
+public class tut6 {
+	public static void main (String args[]){
+		Scanner hey = new Scanner (System.in);
+		
+		int tuna = 5;
+		int tut = 18;
+		++tut;
+		
+	System.out.println(tuna+tut);
+	}
+}
